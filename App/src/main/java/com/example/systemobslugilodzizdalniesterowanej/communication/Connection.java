095671be5b4542d762e448767db770eb4a81d703
Kacper Bielak <github@bielak2000.com>
2024-08-11 12:53:46 +0200

    Revert changes

--- App/src/main/java/com/example/systemobslugilodzizdalniesterowanej/communication/Connection.java
+++ App/src/main/java/com/example/systemobslugilodzizdalniesterowanej/communication/Connection.java
@@ -123,8 +123,8 @@ public class Connection {
                         if (array.length != 2) {
                             log.error(String.format("The wrong received message from boat: %s", readString));
                         } else {
-//                            int messageSign = Integer.parseInt(array[0]);
-//                        receivedMessageHandler(messageSign, array);
+                            int messageSign = Integer.parseInt(array[0]);
+                            receivedMessageHandler(messageSign, array);
                         }
                     } catch (SerialPortException ex) {
                         log.error("Error while receiving data: {}", ex.getMessage());
