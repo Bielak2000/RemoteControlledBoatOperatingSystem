ecc7d08a9b91ef5ab535f92afe61c2ed87a19bb9
Bielak2000 <57075314+Bielak2000@users.noreply.github.com>
2024-12-14 11:37:26 +0100

    Update KalmanFilterAlgorithm.java

--- App/src/main/java/com/example/systemobslugilodzizdalniesterowanej/boatmodel/autonomiccontrol/KalmanFilterAlgorithm.java
+++ App/src/main/java/com/example/systemobslugilodzizdalniesterowanej/boatmodel/autonomiccontrol/KalmanFilterAlgorithm.java
@@ -157,7 +157,7 @@ public class KalmanFilterAlgorithm {
         });
 
         //        // NAJLEPSZE
-                // NIEPEWNOSCI MODELU
+                // NIEPEWNOSCI MODELU, sprobowac zmienic azymut na 2, predkosc katowa na 5
         RealMatrix Q = new Array2DRowRealMatrix(new double[][]{
                 {0.001, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0.001, 0, 0, 0, 0, 0, 0},
@@ -169,7 +169,7 @@ public class KalmanFilterAlgorithm {
                 {0, 0, 0, 0, 0, 0, 0, 0.5}
         });
 
-        // NIEPEWNOSCI POMIAROWE
+        // NIEPEWNOSCI POMIAROWE, sprobowac zmienic azymut na 1, predkosc katowa na 2.5
         RealMatrix R = new Array2DRowRealMatrix(new double[][]{
                 {0.01, 0, 0, 0, 0, 0}, // x
                 {0, 0.01, 0, 0, 0, 0}, // y
